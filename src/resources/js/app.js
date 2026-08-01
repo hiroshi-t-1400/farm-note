86e6345244f2f7f3e47626df394295a644582532
tsukimori hiroshi <hiroshi.tsukimori@gmail.com>
2026-08-01 21:50:55 +0900

    Revert "8 repop make components of other form"

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,15 +1,1 @@
 import './bootstrap';
-
-import Alpine from 'alpinejs';
-// import collapse from '@alpinejs/collapse'; // プラグインのインポート
-// import postForm from './components/postForm';
-import postForm from './components/workLogCreate';
-
-//コンポーネントの登録
-Alpine.data('postForm', postForm);
-
-// AlpineをグローバルなWindowオブジェクトに登録
-window.Alpine = Alpine;
-
-// Alpineの起動
-Alpine.start();
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,19 +1,7 @@
 <?php
 
-use App\Http\Controllers\TestController;
-use App\Http\Controllers\WorkController;
 use Illuminate\Support\Facades\Route;
-
-use Illuminate\Http\Request;
-
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-Route::get('/work-logs/create', [WorkController::class, 'create'])->name('create');
-Route::post('/work-logs/create', [WorkController::class, 'store'])->name('store');
-// Route::post('/create', function (Request $request) {
-//     dd($request);
-// })->name('store');
