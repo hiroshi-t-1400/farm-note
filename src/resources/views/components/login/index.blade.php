9ab438c298f3a8acf25e4fad3d812e42c45aa360
hiroshi-t-1400 <hiroshi.tsukimori@gmail.com>
2026-08-12 09:20:04 +0000

    make Login blade just link to home.
    ログイン画面のレンダリングだけ作成した。
    機能は一切なくホーム画面のダッシュボードに遷移するだけ。
    
    todo 認証機能の実装とログイン認証の動作確認

---
+++ src/resources/views/components/login/index.blade.php
@@ -0,0 +1,18 @@
+{{-- src/resources/views/components/login/index.blade.php --}}
+
+@props([
+    'models' => '',
+    ])
+
+
+<div class="flexn w-md h-sm justify-center">
+
+    <div class="mb-6">
+        <h1 class="text-2xl font-bold text-gray-900">
+            
+        </h1>
+    </div>
+    <div class="">
+
+    </div>
+</div>
